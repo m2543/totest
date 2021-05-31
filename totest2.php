6b697ac30674e2840676bf68654545b2d142e4c4
m2543 <1631051541310@rmutr.ac.th>
2021-05-31 10:18:07 -0700

    Create totest2.php

---
+++ totest2.php
@@ -0,0 +1,22 @@
+
+<HTML>
+
+            <BODY>
+
+               <?
+
+                      echo "This is PHP World";
+
+               ?>
+
+                        <br>
+
+               <?
+
+                      echo "2*2 = ",2*2;
+
+                ?>
+
+        </BODY>
+
+       </HTML>
