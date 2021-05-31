9d1447e5feb94ab001a2cc39ad314b7847642122
m2543 <1631051541310@rmutr.ac.th>
2021-05-31 10:34:51 -0700

    Create totest3.php

---
+++ totest3.php
@@ -0,0 +1,13 @@
+<?php
+
+echo "Bound to pass the point of contemplation.\n";
+echo "Mercury ", "Venus ", "Earth ", "Mars", "\n"; 
+
+$lang = "PHP";
+echo "$lang is a language for web.\n";
+
+$n = 30;
+$n = 6;
+echo "n + n = ", ($n + $n);
+
+?>
